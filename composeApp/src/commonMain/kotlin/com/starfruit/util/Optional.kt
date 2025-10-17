3c1db717d631a92444525ed2b0f1e0c7d8531361
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2025-10-17 09:49:06 +0200

    combine optional api

---
+++ composeApp/src/commonMain/kotlin/com/starfruit/util/Optional.kt
@@ -0,0 +1,46 @@
+package com.starfruit.util
+
+/**
+ * A class that wraps either a concrete value or a value provider, allowing nullable values.
+ * Can be instantiated with a direct value `Optional(value)` or a provider function `Optional { value }`.
+ *
+ * A `Optional<Something>` is equivalent to `Something?`. It is recommended to use `Something?`
+ * whenever possible.
+ *
+ * One use case which `Something?` isn't possible is the `Value<T: Any>` defined
+ * in the decompose library. `Value<Something?>` won't compiled, you will have to use `Value<Optional<Something>>`
+ * as replacement.
+ *
+ * Warning: `Option<Foo>(null) == Option<Bar>(null)` they are `equals` although
+ * the types are different, because they are holding a null value which is types-agnostic.
+ */
+class Optional<out T : Any> private constructor(
+    private val _value: T? = null,
+    private val _provider: (() -> T?)? = null
+) : Any() {
+    /**
+     * Constructs an Optional with a direct value.
+     */
+    constructor(value: T?) : this(_value = value, _provider = null)
+
+    /**
+     * Constructs an Optional with a provider function.
+     */
+    constructor(provider: () -> T?) : this(_value = null, _provider = provider)
+
+    /**
+     * Retrieves the value, either directly or by invoking the provider.
+     */
+    val value: T?
+        get() = _provider?.invoke() ?: _value
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Optional<*>) return false
+        return value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value?.hashCode() ?: 0
+    }
+}
--- composeApp/src/commonTest/kotlin/com/starfruit/navtuto/OptionalTest.kt
+++ composeApp/src/commonTest/kotlin/com/starfruit/navtuto/OptionalTest.kt
@@ -1,22 +1,52 @@
 package com.starfruit.navtuto
 
-import com.starfruit.util.OptionalWrapper
+import com.starfruit.util.Optional
 import kotlin.reflect.KProperty
 import kotlin.test.*
 
 class OptionalTest {
     @Test
-    fun `simple use case`() {
-        val a: OptionalWrapper<Int> = OptionalWrapper { 10 }
+    fun `Optional with concrete value - simple use case`() {
+        val a: Optional<Int> = Optional(10)
         assertEquals(10, a.value)
         assertNotNull(a.value)
 
-        val b: OptionalWrapper<Int> = OptionalWrapper { null }
+        val b: Optional<Int> = Optional { null }
         assertNull(b.value)
     }
 
     @Test
-    fun `optional wrap around lazy`() {
+    fun `Equals hashcode`() {
+        val a1: Optional<Int> = Optional(10)
+        val a2: Optional<Int> = Optional { 10 }
+        val b1: Optional<Int> = Optional { null }
+        val b2 = Optional(null)
+        val s1: Optional<String> = Optional(null)
+        val s2: Optional<String> = Optional { null }
+        val s3: Optional<String> = Optional { "10" }
+
+        assertEquals(a1, a2)
+        assertEquals(b1, b2)
+        assertNotEquals(a1, b2)
+        assertNotEquals(a2, b1)
+
+        assertTrue { b1 == s1 }
+        assertTrue { b1 == s2 }
+        assertFalse { a2 == s3 }
+    }
+
+    @Test
+    fun `Optional with provider - simple use case`() {
+        val a: Optional<Int> = Optional { 10 }
+        assertEquals(10, a.value)
+        assertNotNull(a.value)
+
+        val b: Optional<Int> = Optional { null }
+        assertNull(b.value)
+    }
+
+    @Test
+    fun `Optional with provider - wrap around lazy`() {
         var expensiveCalculationIsCalled = false
         fun expensiveCalculation(): Int {
             expensiveCalculationIsCalled = true
@@ -25,7 +55,7 @@ class OptionalTest {
         //x is a delegate property
         val x by lazy { expensiveCalculation() }
         //a should wrap around x without evaluating it
-        val a: OptionalWrapper<Int> = OptionalWrapper { x }
+        val a: Optional<Int> = Optional { x }
         assertFalse { expensiveCalculationIsCalled } // failed
         // it would only evaluate when the value is access or whenever equals / hashcodes is called
         assertEquals(10, a.value)
@@ -33,7 +63,7 @@ class OptionalTest {
     }
 
     @Test
-    fun `option wrap around other delegate property`() {
+    fun `Optional with provider - other delegate property`() {
         var countRead = 0;
         var countWrite = 0;
         class mydelegate {
@@ -50,7 +80,7 @@ class OptionalTest {
         var x by mydelegate()
         x = 10
         x = 11
-        val a: OptionalWrapper<Int> = OptionalWrapper { x }
+        val a: Optional<Int> = Optional { x }
         assertEquals(1, a.value)
         assertEquals(2, a.value)
         assertEquals(3, a.value)
