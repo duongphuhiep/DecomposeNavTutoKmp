632ee5ab4a156e71597e25bb809272059c08f511
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2025-10-18 19:15:33 +0200

    fix color

--- composeApp/src/commonMain/kotlin/com/starfruit/navtuto/MainView.kt
+++ composeApp/src/commonMain/kotlin/com/starfruit/navtuto/MainView.kt
@@ -3,20 +3,22 @@ package com.starfruit.navtuto
 import AppColors
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MainView(component: MainComponent) {
     Column(modifier = Modifier.background(AppColors.BgMain)) {
         Text("Main")
-        Button(onClick = {component.goBack()}) {
+        Button(onClick = { component.goBack() }) {
             Text("Go Back")
         }
         for (i in 1..3) {
-            Button(onClick = {component.selectItem(i)}) {
+            Button(onClick = { component.selectItem(i) }) {
                 Text("Select Detail $i")
             }
         }
--- composeApp/src/commonMain/kotlin/com/starfruit/navtuto/ui/themes/AppColors.kt
+++ composeApp/src/commonMain/kotlin/com/starfruit/navtuto/ui/themes/AppColors.kt
@@ -1,6 +1,6 @@
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
-    val BgMain = Color(0xf5f3d5)
-    val BgDetails = Color(0xd5e4f5)
+    val BgMain = Color(0xf5f3d500)
+    val BgDetails = Color(0xd5e4f500)
 }
